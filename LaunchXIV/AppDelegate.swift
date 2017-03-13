d329077c2df1fb895a8834389e33e4c45a8aa0bf
Tyrone Trevorrow <tyrone@sudeium.com>
2017-03-13 21:27:44 +1100

    Initial Commit

---
+++ LaunchXIV/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LaunchXIV
+//
+//  Created by Tyrone Trevorrow on 13/3/17.
+//  Copyright © 2017 Tyrone Trevorrow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
