064eda64f0d9254d4fe6fa0b802e1f15c1866a81
Tyrone Trevorrow <tyrone@sudeium.com>
2017-07-18 20:11:41 +1000

    feat(login): login interface

---
+++ LaunchXIV/LoginSettingsWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginSettingsWindowController.swift
+//  LaunchXIV
+//
+//  Created by Tyrone Trevorrow on 18/7/17.
+//  Copyright © 2017 Tyrone Trevorrow. All rights reserved.
+//
+
+import Cocoa
+import OGSwitch
+
+class LoginSettingsWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+    }
+    
+}
